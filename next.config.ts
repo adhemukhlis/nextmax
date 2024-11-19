39abad9dd6652dd76a82a6dcb70bca3cdafc69fa
adhemukhlis <adhemukhlis@gmail.com>
2024-11-19 11:44:39 +0700

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	/* config options here */
+	compiler: {
+		removeConsole: {
+			exclude: ['error', 'warn', 'info']
+		}
+	},
+	poweredByHeader: false,
+	typescript: {
+		ignoreBuildErrors: true
+	},
+	eslint: {
+		ignoreDuringBuilds: true
+	},
+	reactStrictMode: false, // I prefer to set to false to prevent double rendering.
+	outputFileTracingIncludes: {
+		'/': ['./src/**/*']
+	},
+	trailingSlash: false
+	// sassOptions: {
+	// 	includePaths: [path.join(__dirname, 'src', 'styles')]
+	// },
+}
 
-export default nextConfig;
+export default nextConfig
