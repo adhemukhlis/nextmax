b47eb3e1933603217b1d63761596111798f5d3c8
adhemukhlis <adhemukhlis@gmail.com>
2025-02-18 11:22:14 +0700

    [UPDATE] group border

--- src/components/ui/group/index.tsx
+++ src/components/ui/group/index.tsx
@@ -1,10 +1,11 @@
 import { FC, PropsWithChildren } from 'react'
+import _style from './style.module.scss'
 type GroupProps = {
 	label: string
 }
 const Group: FC<PropsWithChildren<GroupProps>> = ({ children, label }) => {
 	return (
-		<fieldset>
+		<fieldset className={_style['group-section']}>
 			<legend>{label}</legend>
 			{children}
 		</fieldset>
