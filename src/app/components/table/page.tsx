2e7d59bb5a010cff30e665fc7b0a1b6f2a0dd091
adhemukhlis <adhemukhlis@gmail.com>
2025-03-13 13:46:10 +0700

    fixing table overflow

--- src/app/components/table/page.tsx
+++ src/app/components/table/page.tsx
@@ -41,48 +41,42 @@ const dataSource: DataSourceType[] = Array.from(Array(16).keys()).map((value) =>
 
 const Page = () => {
 	return (
-		<div className="page column gap-2 page-padding">
+		<div className="page column gap-2 page-padding scroll-x">
 			<ButtonBack
 				navigateTo="/components"
 				label="table"
 			/>
-			<div className="d-flex gap-1">
-				<div
-					className="scroll-x"
-					style={{
-						width: '100%'
-					}}>
-					<table>
-						<thead>
-							<tr>
-								{columns.map((column, index) => (
-									<th
-										key={`header-${index}`}
-										scope="col">
-										{column.title}
-									</th>
-								))}
-							</tr>
-						</thead>
-						<tbody>
-							{dataSource.map((data: DataSourceType, index) => (
-								<tr key={`row-${index}`}>
-									{columns.map((column, _index) =>
-										'scope' in column ? (
-											<th
-												key={`cell-${index}-${_index}`}
-												scope="row">
-												{data[column.dataIndex]}
-											</th>
-										) : (
-											<td key={`cell-${index}-${_index}`}>{data[column.dataIndex]}</td>
-										)
-									)}
-								</tr>
+			<div className="d-flex gap-1 scroll-x">
+				<table>
+					<thead>
+						<tr>
+							{columns.map((column, index) => (
+								<th
+									key={`header-${index}`}
+									scope="col">
+									{column.title}
+								</th>
 							))}
-						</tbody>
-					</table>
-				</div>
+						</tr>
+					</thead>
+					<tbody>
+						{dataSource.map((data: DataSourceType, index) => (
+							<tr key={`row-${index}`}>
+								{columns.map((column, _index) =>
+									'scope' in column ? (
+										<th
+											key={`cell-${index}-${_index}`}
+											scope="row">
+											{data[column.dataIndex]}
+										</th>
+									) : (
+										<td key={`cell-${index}-${_index}`}>{data[column.dataIndex]}</td>
+									)
+								)}
+							</tr>
+						))}
+					</tbody>
+				</table>
 			</div>
 		</div>
 	)
